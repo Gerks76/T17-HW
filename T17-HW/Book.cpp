60f4f1ea0da51facd3f61c83de764110152fa0fd
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-19 20:38:37 +0300

    Class implementation created Book (Book.сpp)

--- T17-HW/Book.cpp
+++ T17-HW/Book.cpp
@@ -1,1 +1,27 @@
 #include "Book.h"
+
+Book::Book()
+{
+    summary = "no summary";
+}
+
+Book::Book(std::string name, std::string author, int year_publishing, std::string summary):Publication(name,author,year_publishing)
+{
+    this->summary = summary;
+}
+
+void Book::set_summary(std::string summary)
+{
+    this->summary = summary;
+}
+
+std::string Book::get_summary() const
+{
+    return summary;
+}
+
+void Book::show() const
+{
+    Publication::show();
+    std::cout << "Summary: " << summary << std::endl;
+}
