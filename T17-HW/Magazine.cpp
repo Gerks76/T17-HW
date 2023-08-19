0148c60216406d961f8ce6066cab7b51507fe1e9
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-19 21:06:13 +0300

    Class implementation created Magazine (Magazine.сpp)

--- T17-HW/Magazine.cpp
+++ T17-HW/Magazine.cpp
@@ -1,1 +1,27 @@
 #include "Magazine.h"
+
+Magazine::Magazine()
+{
+    articles = "no articles";
+}
+
+Magazine::Magazine(std::string name, std::string author, int year_publishing, std::string articles):Publication(name, author, year_publishing)
+{
+    this->articles = articles;
+}
+
+void Magazine::set_articles(std::string articles)
+{
+    this->articles = articles;
+}
+
+std::string Magazine::get_articles() const
+{
+    return articles;
+}
+
+void Magazine::show() const
+{
+    Publication::show();
+    std::cout << "Articles: " << articles << std::endl;
+}
