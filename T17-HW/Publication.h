93790fd7e9767ab21e3304cb01d9aca109227a32
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-19 20:13:21 +0300

    Created a prototype of the Publication class (Publication.h)

---
+++ T17-HW/Publication.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+class Publication
+{
+protected:
+	std::string name;
+	std::string author;
+	int year_publishing;
+public:
+	Publication();
+	Publication(std::string name, std::string author, int year_publishing);
+
+	void set_name(std::string name);
+	void set_author(std::string author);
+	void set_year_publishing(int year_publishing);
+
+	std::string get_name()const;
+	std::string get_author()const;
+	int get_year_publishing()const;
+
+	virtual void show()const;
+};
+
