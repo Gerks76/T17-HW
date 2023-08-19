1a71506d1a5abc05fa99c352d4faaa9db18eabdb
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-19 21:26:48 +0300

    Class prototype created Library (Library.h)

---
+++ T17-HW/Library.h
@@ -0,0 +1,24 @@
+#pragma once
+#include"Book.h"
+#include"Magazine.h"
+#include"Publication.h"
+#include<vector>
+
+class Library
+{
+	std::string name_library;
+	std::vector<Publication*>library;
+public:
+	Library();
+	Library(std::string name_library);
+	~Library();
+
+	void set_name_library(std::string name_library);
+	std::string get_name_library()const;
+
+	void show()const;
+	void add_publication(Publication* obj);
+	void del_publication(std::string name)const;
+	void find_publication(std::string name)const;
+};
+
